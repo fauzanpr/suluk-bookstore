973cc5a99c8566aa41d5474ff6e23db72cb15c87
AhmadRafif22 <rafifayuzaki123@gmail.com>
2022-06-13 00:43:50 +0700

    landingpage- proggress

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.layout.masterlayout');
+    return view('landingpage');
 });
 
 // ROUTE FOR ADMIN
